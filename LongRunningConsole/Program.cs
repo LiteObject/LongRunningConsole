194d33e59d5d1edf0b8e4b8aca440b0841cb2236
Mohammed <tuniis@live.com>
2019-02-08 21:19:48 -0600

    Add project files.

---
+++ LongRunningConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LongRunningConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
