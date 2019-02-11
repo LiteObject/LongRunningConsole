e8097af8bdc61b18aed5ece0a297e2cee7d94ba2
Mohammed <tuniis@live.com>
2019-02-10 21:10:33 -0600

    Initial create

---
+++ LongRunningConsole/MyService.cs
@@ -0,0 +1,87 @@
+﻿namespace LongRunningConsole
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// The my service.
+    /// </summary>
+    public class MyService : IHostedService, IDisposable
+    {
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        private readonly ILogger logger;
+
+        /// <summary>
+        /// The _timer.
+        /// </summary>
+        private Timer timer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyService"/> class.
+        /// </summary>
+        /// <param name="logger">
+        /// The logger.
+        /// </param>
+        public MyService(ILogger<MyService> logger)
+        {
+            this.logger = logger;
+        }
+
+        /// <summary>
+        /// The start async.
+        /// </summary>
+        /// <param name="cancellationToken">
+        /// The cancellation token.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"{DateTime.Now} Timed Background Service is starting.");
+            this.timer = new Timer(this.DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+            return Task.CompletedTask;
+        }
+        
+        /// <summary>
+        /// The stop async.
+        /// </summary>
+        /// <param name="cancellationToken">
+        /// The cancellation token.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"{DateTime.Now} Timed Background Service is stopping.");
+            this.timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+        
+        /// <summary>
+        /// The dispose.
+        /// </summary>
+        public void Dispose()
+        {
+            this.timer?.Dispose();
+        }
+        
+        /// <summary>
+        /// The do work.
+        /// </summary>
+        /// <param name="state">
+        /// The state.
+        /// </param>
+        private void DoWork(object state)
+        {
+            Console.WriteLine($"{DateTime.Now} Timed Background Service is working.");
+        }
+    }
+}
--- LongRunningConsole/Program.cs
+++ LongRunningConsole/Program.cs
@@ -1,12 +1,72 @@
-﻿using System;
-
-namespace LongRunningConsole
+﻿namespace LongRunningConsole
 {
-    class Program
+    using System;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// The program.
+    /// Original Article: https://github.com/stevejgordon/GenericHostExample/blob/master/GenericHostExample/Program.cs
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var watch = new Stopwatch();
+            watch.Start();
+
+            await new HostBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                    {
+                        config.AddJsonFile("appsettings.json", optional: true);
+                        /*config.AddEnvironmentVariables();
+
+                        if (args != null)
+                        {
+                            config.AddCommandLine(args);
+                        }*/
+                    })
+                .ConfigureServices(
+                    (hostContext, services) =>
+                        {
+                            services.AddOptions();
+                            services.AddLogging();
+                            services.AddHostedService<MyService>();
+                        })
+                .ConfigureLogging(
+                    (hostContext, services) =>
+                        {
+                            services.AddConsole();
+                        })
+                .RunConsoleAsync();
+            
+            watch.Stop();
+            Print($"\n\nPress any key to exit. Elapsed: {watch.Elapsed}");
+        }
+
+        /// <summary>
+        /// The print.
+        /// </summary>
+        /// <param name="message">
+        /// The message.
+        /// </param>
+        private static void Print(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
